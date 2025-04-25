b4a90a2cdc60d23cf6cb5e27fef3a6106ac1c30a
ryo273917 <ttc2350sg0009@edu.tech.ac.jp>
2025-04-25 17:39:47 +0900

    plaer2
    スクリプトを統合

--- Assets/Member/Otake/Scripts/PlayerMover.cs
+++ Assets/Member/Otake/Scripts/PlayerMover.cs
@@ -7,9 +7,11 @@ public class PlayerMover : MonoBehaviour
     private Rigidbody2D rb;
     private float speed = 1.0f;
     private Vector2 direction;
+    CircleCollider2D circleCol;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        circleCol = GetComponent<CircleCollider2D>();
     }
     void Update()
     {
@@ -27,5 +29,28 @@ public class PlayerMover : MonoBehaviour
     {
         Vector2 dist = direction * speed * Time.fixedDeltaTime;
         rb.MovePosition(rb.position + dist);
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Map")
+        {
+            Debug.Log("Map Hit");
+            circleCol.enabled = true;
+            Debug.Log("Atack!");
+            StartCoroutine("WaitTime");
+
+        }
+        else
+        {
+            Debug.Log("Enemy Hit");
+            circleCol.enabled = false;
+            Debug.Log("End2");
+        }
+    }
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSeconds(1);
+        circleCol.enabled = false;
+        Debug.Log("End");
     }
 }
