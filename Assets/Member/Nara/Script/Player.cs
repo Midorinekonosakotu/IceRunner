3e70d60942cf29b0a3a4e95f38ccb065d282c17a
Midorinekonosakotu <midorineko10801@gmail.com>
2025-04-25 11:00:05 +0900

    TestPlayerの追加

---
+++ Assets/Member/Nara/Script/Player.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float speed = 8.0f;
+    private Vector2 direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            direction.x = Input.GetAxisRaw("Horizontal");
+            direction.y = Input.GetAxisRaw("Vertical");
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 dist = direction * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + dist);
+    }
+}
