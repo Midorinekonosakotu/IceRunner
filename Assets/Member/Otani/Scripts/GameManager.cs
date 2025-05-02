1cc73edb398bb4c57c91a3d5f9ec3ac2d9d27352
soukenbichappp <soukenbichappp@gmail.com>
2025-05-02 18:24:36 +0900

    GameManagerとタイトルシーン
    仮UIを配置し、GameManagerに複数の処理を書きました。

--- Assets/Member/Otani/Scripts/GameManager.cs
+++ Assets/Member/Otani/Scripts/GameManager.cs
@@ -1,16 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    [SerializeField] int Score;
-    // [SerializeField] GameObject player; プレイヤーが完成したら追加
+    //[SerializeField] Player player; プレイヤーが完成したら追加
     public static GameManager instance = null;
-
+    public int Score = 0;
 
     private void Awake()
     {
+        // シングルトン
         if (instance == null)
         {
             instance = this;
@@ -21,15 +22,33 @@ public class GameManager : MonoBehaviour
             Destroy(this.gameObject);
         }
     }
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+        /* 敵と当たった時の処理
+        if(player.isHit == true)
+        {
+            GameOver();
+        }*/
+    }
+
+    void GameOver()
+    {
+        // GameOver処理
+        // GameOverアニメーションをここに入れる
+        SceneManager.LoadScene("");
+    }
+
+    public void OpenSetting()
+    {
 
     }
+
 }
