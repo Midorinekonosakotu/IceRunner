fdd3af2daa30be1e50aab20039c097ce761b05a4
Midorinekonosakotu <midorineko10801@gmail.com>
2025-05-09 13:09:07 +0900

    ボタンの追加のみ

---
+++ Assets/Member/Nara/Script/PauseButtonScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButtonScript : MonoBehaviour
+{
+    // Pause 用のボタン
+    [SerializeField] private Button PauseButton;
+    // ポーズされているかどうか
+    private bool isPause = false;
+
+    private void Update()
+    {
+        
+    }
+
+    public void OnButton()
+    {
+
+    }
+}
